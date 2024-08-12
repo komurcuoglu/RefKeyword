da9c74cfb4db8c9784905f30618c6ac4d1f5a831
komurcuoglu <komurcuoglu@MEHMET>
2024-08-13 02:02:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ParamsKeyword
+{
+    class Program
+    {
+        static int Add(params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+
+        static void Main(string[] args)
+        {
+            var result = Add(3, 5,100);
+            Console.WriteLine(result);
+        }
+    }
+}
